f4ab7101886f419682f02389c80d8f4e186d3995
John Hearn <hearn.john@gmail.com>
2019-07-28 14:23:37 +0200

    Oracle debug output is optional

--- src/Oracle.jl
+++ src/Oracle.jl
@@ -20,19 +20,21 @@ permmat(π) = sparse(1:length(π), π, 1)
 @test permmat(permutate(2, 1=>2)) == [0 1; 1 0]
 @test permmat(permutate(4, 1=>2, 3=>4)) == [0 1 0 0; 1 0 0 0; 0 0 0 1; 0 0 1 0]
 
-oracle(m::Int, n::Int, f) = begin
+format(x::Int, width::Int) = string(x,base=2,pad=width)
+
+oracle(m::Int, n::Int, f, debug = false) = begin
     perm = Dict{Int,Int}()
     for x in 0:2^m-1
         for y in 0:2^n-1
             xy = (x << n) + y      # |x>|y>
             fxy = f(x, y)          # |f(x,y)>
             xfxy = (x << n) + fxy  # |x>|f(x,y)>
-            #println(format(x, m)," ",format(y, n)," ",format(x, m)," ",format(fxy, n)," | $xy => $xfxy")
+            if debug
+                println(format(x, m)," ",format(y, n)," | ",format(x, m)," ",format(fxy, n)," | $xy => $xfxy")
+            end
             perm[xy+1] = xfxy+1
         end
     end
     #permmat(permutate(2^(n+m), map...))
     permmat(map((it) -> perm[it], 1:2^(n+m)))
 end
-
-format(x::Int, width::Int) = string(x,base=2,pad=width)
